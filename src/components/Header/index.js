48c4f82a21ef88ff4c7fdffcb63e0e303e89772a
ilhoon <rival6821@naver.com>
2019-05-06 11:22:13 +0900

    style 헤더 기초작업

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import classNames from 'classnames/bind';
+import styles from 'styles/common.scss';
+const cx = classNames.bind(styles);
 
 const Header = () => {
     return (
-        <div>
-
+        <div className={cx('header')}>
+            <div className={cx('logo')}>
+                로고
+            </div>
+            <div className={cx('login')}>
+                로그인
+            </div>
         </div>
     );
 }
