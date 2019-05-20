b3f1c69a159aa5265c138f8dbef437ea17366059
ilhoon <rival6821@naver.com>
2019-05-20 16:09:35 +0900

    아 어렵다. 모르겟다

--- components/Header.js
+++ components/Header.js
@@ -1,43 +1,46 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-const styles = {
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
-  grow: {
+  title: {
     flexGrow: 1
   }
-};
+}));
 
 const dummy = {
   isLoggedIn: false
 };
 
-const Header = ({ classes }) => {
+const Header = () => {
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
+          <Typography variant="h6" color="inherit" className={classes.title}>
             후니보드
           </Typography>
-          <Button color="inherit">로그인</Button>
-          <Button color="inherit">회원가입</Button>
+          {dummy.isLoggedIn ? (
+            <></>
+          ) : (
+            <>
+              <Button color="inherit">로그인</Button>
+              <Button color="inherit">회원가입</Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>
   );
 };
 
-Header.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(Header);
+export default Header;
